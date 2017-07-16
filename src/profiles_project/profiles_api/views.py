34ec460d3f6f5b751ff0149124119e2e7b929a3c
Zacharias <z.hadji02@gmail.com>
2017-07-16 21:42:35 +0300

    Added our first viewset and router

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -43,7 +44,6 @@ class HelloApiView(APIView):
 
         return Response({'method': 'put'})
 
-
     def patch(self, request, pk=None):
         """Handles updating an object."""
 
@@ -53,3 +53,18 @@ class HelloApiView(APIView):
         """Deletes an object."""
 
         return Response({'method': 'delete'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message."""
+
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial update',
+            'Automatically maps URLs using Routers',
+            'Provides more functionality with less code.'
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
