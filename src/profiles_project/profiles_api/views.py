c960f98e5bb1a24ecfc4c7c5cd9c8da79f2e3741
Zacharias <z.hadji02@gmail.com>
2017-07-23 11:10:33 +0300

    profiles listing and updating

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from . import serializers
-
+from . import models
 
 # Create your views here.
 
@@ -102,3 +102,9 @@ class HelloViewSet(viewsets.ViewSet):
         """Handles removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, listing and updating profiles."""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
